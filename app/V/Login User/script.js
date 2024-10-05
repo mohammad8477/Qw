f6e3c913f62923a4e62ae32e3ff13f7c0581dda7
Mohammad <M137784M@gmail.com>
2024-10-05 11:24:08 +0330

    1

---
+++ app/V/Login User/script.js
@@ -0,0 +1,71 @@
+function Strength(password) {
+  let i = 0;
+  if (password.length > 6) {
+    i++;
+  }
+  if (password.length >= 10) {
+    i++;
+  }
+
+  if (/[A-Z]/.test(password)) {
+    i++;
+  }
+
+  if (/[0-9]/.test(password)) {
+    i++;
+  }
+
+  if (/[A-Za-z0-8]/.test(password)) {
+    i++;
+  }
+
+  return i;
+}
+
+let container = document.querySelector(".container");
+document.addEventListener("keyup", function (e) {
+  let password = document.querySelector("#YourPassword").value;
+
+  let strength = Strength(password);
+  if (strength <= 2) {
+    container.classList.add("weak");
+    container.classList.remove("moderate");
+    container.classList.remove("strong");
+  } else if (strength >= 2 && strength <= 4) {
+    container.classList.remove("weak");
+    container.classList.add("moderate");
+    container.classList.remove("strong");
+  } else {
+    container.classList.remove("weak");
+    container.classList.remove("moderate");
+    container.classList.add("strong");
+  }
+});
+
+let password = document.querySelector("#YourPassword");
+let show = document.querySelector(".show");
+show.onclick = function () {
+  if (password.type === "password") {
+    password.setAttribute("type", "text");
+    show.classList.add("hide");
+  } else {
+    password.setAttribute("type", "password");
+    show.classList.remove("hide");
+  }
+};
+function togglePassword() {
+  var passwordInput = document.getElementById("YourPassword");
+  var eyeIcon = document.querySelector(".eye-icon i");
+
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    eyeIcon.classList.remove("fa-eye");
+    eyeIcon.classList.add("fa-eye-slash");
+  } else {
+    passwordInput.type = "password";
+    eyeIcon.classList.remove("fa-eye-slash");
+    eyeIcon.classList.add("fa-eye");
+  }
+}
+
+
