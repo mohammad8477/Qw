4ae677259e7ab644249853d941965ab22df58945
mrmp98 <amirmp2332@gmail.com>
2024-10-06 20:11:59 +0330

    amniyat aded

---
+++ app/V/Login User/amniyat.php
@@ -0,0 +1,10 @@
+<?php 
+
+class Amniya
+{
+    public function __construct()
+    {
+        return (isset()) ?($kapcha == true)?(!empty()&& !empty()) ? ($etelaat == true ) : '' :' ' :' ' ; 
+       
+    }
+}
