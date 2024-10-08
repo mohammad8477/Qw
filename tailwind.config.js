e67882bae71b03a4a8f6d695035577e0145ddf0d
Mohammad <M137784M@gmail.com>
2024-10-08 15:15:52 +0330

    Forgot pass

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/V/Forgot password/Enter p Or e/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
