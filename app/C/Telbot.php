fac37848eef6b20ec2cbcc7068d8e9aa0ce2b171
arefjami <amirmp2332@gmail.com>
2024-10-08 23:10:06 +0330

    tel bot is finish

--- app/C/Telbot.php
+++ app/C/Telbot.php
@@ -3,40 +3,7 @@ namespace App\C;
 
 class Telbot 
 {
-    private $token;
-    private $chatId;
-
-    public function __construct($token, $chatId) 
-    {
-        $this->token = $token;
-        $this->chatId = $chatId;
-        $this->sendMessage("پیام شما به چت آیدی $chatId ارسال شد!");
-    }
-
-    private function sendMessage($message) 
-    {
-        $url = "https://tapi.bale.ai/bot{$this->token}/sendMessage";
-        $data = [
-            'chat_id' => $this->chatId,
-            'text' => 'opvsjdvpoidsjvdsoivdsjvodsivsd',
-        ];
-
-        // ارسال درخواست به TAPI
-        $options = [
-            'http' => [
-                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-                'method'  => 'POST',
-                'content' => http_build_query($data),
-            ],
-        ];
-        $context  = stream_context_create($options);
-        $result = file_get_contents($url, false, $context);
-
-        // بررسی نتیجه
-        if ($result === FALSE) {
-            die('Error occurred while sending message.');
-        }
-    }
+   public $token = '1075635394:cMwyIdMUtP5EcM6Do11JTLk5YxoBx3RR9pK8dABS'  ; 
+   public $chatid= 217333473 ;
+   public function __construct() { $url = "https://tapi.bale.ai/bot{$this->token}/sendMessage"; $data = ['chat_id' => $this->chatid, 'text' => rand(100000000,999999999999999999)]; $context = stream_context_create([ 'http' => [ 'header' => "Content-Type: application/x-www-form-urlencoded\r\n", 'method' => 'POST', 'content' => http_build_query($data), ], ]); file_get_contents($url, false, $context); }
 }
-
-// استفاده از کلاس Telbot
