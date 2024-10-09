e7371bf654b96f5ec967e009819887a70a0a373f
mrmp98 <amirmp2332@gmail.com>
2024-10-09 18:34:19 +0330

    root aded

--- public/index.php
+++ public/index.php
@@ -1,3 +1,15 @@
 <?php 
 require_once __DIR__ . '/../botstrap.php' ; 
-use App\C\CaptchaGenerator ;
+
+$darkhast =  $_SERVER['REQUEST_METHOD']  ; 
+ echo $url = trim(substr($_SERVER['REQUEST_URI'] , 9))  ;
+$routes = [
+    'GET' => [
+        "" => ['controller' => 'app\controllers\HomeController' , 'method' => 'index'],
+
+        "posts" => ['controller' => 'app\controllers\PostController' , 'method' => 'index'],
+        "posts/show/([0-9]+)" => ['controller' => 'app\controllers\PostController' , 'method' => 'show']
+    ],
+    'POST' => []
+];
+
