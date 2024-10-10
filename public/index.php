d952198a9878ac63d362e5bc8676c4a36b39cb3c
mrmp98 <amirmp2332@gmail.com>
2024-10-10 15:58:17 +0330

    root aded

--- public/index.php
+++ public/index.php
@@ -1,4 +1,13 @@
 <?php 
 require_once __DIR__ . '/../botstrap.php' ; 
 $pash  = trim( str_replace( '/mame/Qw/','' ,parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)), '/') ;  
-
+$method =
+[
+    $GET = [
+        'code'   => ['CODE' , 'index']   , 
+        'signin' => [ 'SIGIN' , 'index'] , 
+        'signup' => ['SIGNUP' , 'index']
+        
+    ]  ,   
+    $post = []
+] ; 
