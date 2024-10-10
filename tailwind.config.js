11c5dc93ddc89ee792f6da479c505420654fb62f
Mohammad <M137784M@gmail.com>
2024-10-10 14:59:08 +0330

    Tailwind Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: ["./app/V/Forgot password/Enter the code/**/*.{html,js}",
     "./app/V/Forgot password/Enter p Or e/**/*.{html,js}",
-    "./app/V/Forgot password/Rest password/**/*.{html}"
+    "./app/V/Forgot password/Rest password/**/*.html"
   ],
   theme: {
     extend: {},
