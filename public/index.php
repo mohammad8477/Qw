b9427d3b85eb965e1bda9ba0868cd69dd0bee54d
arefjami <amirmp2332@gmail.com>
2024-10-10 07:44:47 +0330

    roter aded

--- public/index.php
+++ public/index.php
@@ -1,15 +1,4 @@
 <?php 
 require_once __DIR__ . '/../botstrap.php' ; 
-
-$darkhast =  $_SERVER['REQUEST_METHOD']  ; 
- echo $url = trim(substr($_SERVER['REQUEST_URI'] , 9))  ;
-$routes = [
-    'GET' => [
-        "" => ['controller' => 'app\controllers\HomeController' , 'method' => 'index'],
-
-        "posts" => ['controller' => 'app\controllers\PostController' , 'method' => 'index'],
-        "posts/show/([0-9]+)" => ['controller' => 'app\controllers\PostController' , 'method' => 'show']
-    ],
-    'POST' => []
-];
+$pash  = trim( str_replace( '/mame/Qw/','' ,parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)), '/') ;  
 
