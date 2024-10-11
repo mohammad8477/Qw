3fd3a8c0604fab6130bc8bff67957b067deeda43
arefjami <amirmp2332@gmail.com>
2024-10-11 09:15:35 +0330

    root aded

--- public/index.php
+++ public/index.php
@@ -1,4 +1,27 @@
 <?php 
-require_once __DIR__ . '/../botstrap.php' ; 
+require_once __DIR__ . '/../botstrap.php' ;
+$q = false ;  
 $pash  = trim( str_replace( '/mame/Qw/','' ,parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)), '/') ;  
 
+$routes = 
+[
+    'GET' => 
+    [
+          'code' =>['controller'=> 'app/C/code.php' , 'method' =>'index'] , 
+         'sigin' =>['controller'=> 'SIGIN' , 'method' =>'index'] ,  
+         'sigup' =>['controller'=> 'SIGUP' , 'method' =>'index'] ,  
+          'index' =>['controller'=> 'SAFE' , 'method' =>'index'] ,   
+"index/buy/([0-9]+)" =>['controller'=> 'BAY' , 'method' =>'index'] , 
+         'admin' =>['controller'=> 'ADMIN' , 'method' =>'index'] ,
+
+    ] , 
+    'POST' =>[]
+] ; 
+foreach($routes[$_SERVER['REQUEST_METHOD']] as $route=>$info){
+    if(preg_match("#^$route$#" , $pash)){
+        echo "ok" ; 
+        $q = true ; 
+        break ; 
+    }
+}
+echo (!($q== 1)) ? '404' : ' ' ; 
